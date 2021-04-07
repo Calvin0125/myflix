f0193e4ca4ce7e4de48506f1a975fbc784f35b05
Calvin Conley <cconley0125@gmail.com>
2021-04-07 15:33:11 -0400

    added category model and show category page

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_05_202251) do
+ActiveRecord::Schema.define(version: 2021_04_07_190323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "videos", force: :cascade do |t|
     t.string "title"
@@ -22,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_04_05_202251) do
     t.string "small_cover_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
   end
 
 end
