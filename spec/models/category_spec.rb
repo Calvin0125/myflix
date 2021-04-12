a8db5a70aff03ce9f428aa358ceed6725aca24a1
Calvin Conley <cconley0125@gmail.com>
2021-04-12 19:37:12 -0400

    updated code to use shoulda matchers

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,24 +1,4 @@
 require 'rails_helper'
 describe Category do
-  before(:each) do
-  @sci_fi = Category.create(name: "Sci-fi")
-  @star_wars = Video.create(title: "Star Wars", 
-                         description: "Battles in space", 
-                         large_cover_url: "https://via.placeholder.com/500x350.png?text=Star+...", 
-                         small_cover_url: "https://via.placeholder.com/200x300.png?text=Star+...", 
-                         category_id: Category.first.id)
-  @star_trek = Video.create(title: "Star Trek",
-                         description: "Space Voyages",
-                         large_cover_url: "https://via.placeholder.com/500x350.png?text=Star+Trek", 
-                         small_cover_url: "https://via.placeholder.com/200x300.png?text=Star+Trek",
-                         category_id: Category.first.id)
-  end
-
-  it "should save" do
-    expect(Category.first).to eq(@sci_fi)
-  end
-
-  it "can have many videos" do
-    expect(@sci_fi.videos).to include(@star_wars, @star_trek)
-  end
+  it { should have_many :videos }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,30 +1,12 @@
 require 'rails_helper'
 
 describe Video do
-  before(:each) do 
-    @sci_fi = Category.create(name: "Sci-fi")
-    @star_wars = Video.create(title: "Star Wars", 
-                       description: "Battles in space", 
-                       large_cover_url: "https://via.placeholder.com/500x350.png?text=Star+Wars", 
-                       small_cover_url: "https://via.placeholder.com/200x300.png?text=Star+Wars", 
-                       category_id: Category.first.id)
+  describe 'associations' do
+    it { should belong_to :category }
   end
 
-  it 'should save' do
-    expect(Video.first).to eq(@star_wars)
-  end
-
-  it 'should belong to Sci-fi category' do
-    expect(@star_wars.category).to eq(@sci_fi)
-  end
-
-  it 'should not save without title' do
-    video = Video.new(description: 'test movie', category_id: Category.first.id)
-    expect(video.save).to eq(false)
-  end
-
-  it 'should not save without description' do
-    video = Video.new(title: 'test movie', category_id: Category.first.id)
-    expect(video.save).to eq(false)
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :description }
   end
 end
