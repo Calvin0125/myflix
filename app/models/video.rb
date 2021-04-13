569c99b128414cb3bb6dadbd41ef8a8bcdabdc86
Calvin Conley <cconley0125@gmail.com>
2021-04-13 08:55:28 -0400

    updated video model so category id is optional

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,8 @@
 class Video < ActiveRecord::Base
-  belongs_to :category
+  belongs_to :category, optional: true
   validates_presence_of :title, :description
+
+  def self.search_by_title(search_term)
+
+  end
 end
