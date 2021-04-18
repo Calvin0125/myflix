14dab57eaeb41b042ee4c63acce540937b915a41
Calvin Conley <cconley0125@gmail.com>
2021-04-18 11:10:12 -0400

    added user_spec to test user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper.rb'
+
+describe User do
+  describe "security" do
+    it { should have_secure_password }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :full_name }
+    it { should validate_presence_of :password }
+    it { should validate_uniqueness_of :email }
+  end
+end
